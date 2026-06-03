dc81e0692f95870e3e40b937b10daab35a256bb8
sreeja-create <alesreeja1@gmail.com>
2026-06-03 20:40:59 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,4 +3,9 @@ list.append(4)
 print(list) # add element at end
 list.sort()
 print(list) # arranges in ascending order
+list.sort(reverse=true)
+print(list) # arrangesin descending order
+list.reverse()
+print(list)
+
 
