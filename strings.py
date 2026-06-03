08f0239e14c69baac98d17c310f835762b83f6a0
sreeja-create <alesreeja1@gmail.com>
2026-06-03 17:14:26 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -8,3 +8,12 @@ strl="sreeja"
 print(str.capitalize())
 output:-Sreeja.  #only 1st letter changes 
 ‐-------------------------------
+str.replace():-
+strl="I am sreeja"
+print(str.replace("a","o"))
+‐------------------------------
+str.find():-
+strl="I am sreeja"
+print(str.find("r"))
+--‐-------------------------------
+‐------------------------------
