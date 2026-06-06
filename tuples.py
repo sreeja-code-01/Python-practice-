5777244bd8ceb9fda973e3669ea04d15f23cc915
sreeja-create <alesreeja1@gmail.com>
2026-06-06 22:27:28 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,12 @@
+tup=(10,20,30,40)
+tup[0]=50
+print(tup) #not possible immutable
+‐-------------------------------
+tuple methods:-
+tup.ind(el) # returns ind3x pf 1st occurance 
+tup.count(el)
+tup.max()
+tup.min()
+tup.sum()
+tup.len()
+‐-------------------------------
