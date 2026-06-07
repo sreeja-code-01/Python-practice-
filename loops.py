28dbaa1272a16c64c779d8e97d1597234aff5ae0
sreeja-create <alesreeja1@gmail.com>
2026-06-07 21:27:12 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+while loop
+while cond:
+#some work
+-------------
+print no.feom 1to 5
+num = 1
+
+while num <= 5:
+    print(num)
+    num += 1
+-------------
+print no.s from 5 to 1
+num = 5
+
+while num >= 1:
+    print(num)
+    num -= 1
+-----------
