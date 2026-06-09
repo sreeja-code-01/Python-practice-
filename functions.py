c703fd67ff342ba75f3df3c2cf7710c48d9f9374
sreeja-create <alesreeja1@gmail.com>
2026-06-09 15:56:20 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,20 @@
+1st method with parameters:
+def funt_name(para):
+    print(para)
+funct_name(value)
+
+def add(a,b):
+    print(a+b)
+add(5,6)
+output:'11
+---------
+def name(n):
+   print(n)
+name("sree")
+output:-sreeja
+---------
+def name(n)
+   print("hello im",n)
+name("sree")
+output:- hello im sree
+--------
