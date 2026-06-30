58f9181373c51832607dc0de4248549fa534811f
Sreeja.A <alesreeja1@gmail.com>
2026-06-30 19:14:44 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -3,3 +3,19 @@ l=[1,2,5,2,9,5,6,2,9]
 s=set(l)
 print(len(s))
 ---------------
+#find most frequenet element
+l=[1,2,4,2,1,5,7,2,4,4,4,4]
+s={}
+for i in l:
+    if i in s:
+        s[i]=s[i]+1
+    else:
+        s[i]=1
+h=0
+e=0
+for i in s:
+    if s[i]>h:
+      h=s[i]
+      e=i                    
+print(e)    
+-----------
