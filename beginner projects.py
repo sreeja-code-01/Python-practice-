f8db210a4887e34952dde9d0ef2965aed6072b9e
Sreeja.A <alesreeja1@gmail.com>
2026-07-03 06:22:04 +0530

    Update beginner projects.py

--- beginner projects.py
+++ beginner projects.py
@@ -1,1 +1,35 @@
+#rock,paper,scissor game
+import random
+choose=input("rock,paper,scissor(r/p/s)= ").lower()
+comp=random.choice(["r","p","s"])
+print(comp)
+if choose=="r" and comp=="p":
+    print("u won")
+elif choose=="r" and comp=="s":
+    print("u won")
+elif choose=="r" and comp=="r":
+    print("draw")
+elif choose=="p" and comp=="r":
+    print("comp won")
+elif choose=="p" and comp=="s":
+    print("comp won")
+elif choose=="p" and comp=="p":
+    print("draw")
+elif choose=="s" and comp=="r":
+    print("comp won")
+elif choose=="s" and comp=="p":
+    print("u wonwon")
+elif choose=="s" and comp=="s":
+    print("drwa")
+else:
+    print("invalid choice")    
+----------------------
+
+
+
+
+
+
+
+
 
